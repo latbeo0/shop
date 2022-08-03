568db6a918136489e4beeb13c1d7766c802724b2
LV7 <luonanhtuan2000bh@gmail.com>
2022-08-03 12:32:21 +0700

    update v2

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,12 +2,7 @@ import './app.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
